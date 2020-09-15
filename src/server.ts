bf89f6f9979282071b80acc0b8bdb104fd8fbc33
Nithin Saji <nithin111@gmail.com>
2020-09-16 02:30:53 +0800

    setup swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import * as md_auth from './middlewares/authenticate';
 
 import logger from 'morgan';
 import fastify from 'fastify';
+import fastifySwagger from 'fastify-swagger'
 import cors from 'cors';
+import middie from 'middie'
 
 export class Server {
 
@@ -17,18 +19,20 @@ export class Server {
 
     constructor(private port: number) {
         this.app = fastify();
-        this.config();
-        this.api();
+        
     }
 
-    start() {
-        return new Promise( (resolve, reject) => {
-            this.app.listen(this.port, '0.0.0.0', (err: any) => {
-                if (err) { reject(err); return; }
-                console.log(`server listening on ${this.port}`);
-                resolve();
-            });
-        });
+    async start() {
+
+        await this.config(); 
+        await this.api();
+        try {
+            await this.app.listen(this.port, '0.0.0.0')
+            console.log(`server listening on ${this.port}`);
+        }
+        catch (e) {
+            console.log(e)
+        }
     }
 
     public api() {
@@ -38,11 +42,17 @@ export class Server {
 
         this.app.post('/register', this.opts, UserService.registerUser);
         this.app.get('/public', MainService.getPublic);
-        this.app.get('/private', {preHandler: [md_auth.ensureAuth]}, MainService.getPrivate);
+        this.app.get('/private', { preHandler: [md_auth.ensureAuth] }, MainService.getPrivate);
+
+
 
     }
 
-    public config() {
+    public async config() {
+
+        await this.app.register(middie)
+        console.log("registered middie for handling middleware")
+
 
         const options: cors.CorsOptions = {
             allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token'],
@@ -55,6 +65,50 @@ export class Server {
         this.app.use(cors(options));
 
         this.app.use(logger('dev'));
+
+
+        this.app.register(fastifySwagger, {
+            routePrefix: '/documentation',
+            swagger: {
+                info: {
+                    title: 'Test swagger',
+                    description: 'testing the fastify swagger api',
+                    version: '0.1.0'
+                },
+                externalDocs: {
+                    url: 'https://swagger.io',
+                    description: 'Find more info here'
+                },
+                host: 'localhost',
+                schemes: ['http'],
+                consumes: ['application/json'],
+                produces: ['application/json'],
+                tags: [
+                    { name: 'user', description: 'User related end-points' },
+                    { name: 'code', description: 'Code related end-points' }
+                ],
+                definitions: {
+                    User: {
+                        type: 'object',
+                        required: ['id', 'email'],
+                        properties: {
+                            id: { type: 'string', format: 'uuid' },
+                            firstName: { type: 'string' },
+                            lastName: { type: 'string' },
+                            email: { type: 'string', format: 'email' }
+                        }
+                    }
+                },
+                securityDefinitions: {
+                    apiKey: {
+                        type: 'apiKey',
+                        name: 'apiKey',
+                        in: 'header'
+                    }
+                }
+            },
+            exposeRoute: true
+        })
     }
 
 }
