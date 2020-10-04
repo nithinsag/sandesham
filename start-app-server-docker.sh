f3ec87fa4993c925c5fe1894b8e82862780de753
Nithin Saji <nithin111@gmail.com>
2020-10-04 18:52:38 +0800

    add deploy script to git

--- start-app-server-docker.sh
+++ start-app-server-docker.sh
@@ -1,8 +1,11 @@
 # to start application from docker container
 # add additional environment variables for docker here
 
-docker build --tag sandesham/1.0 .
+
+VERSION="1.0.7"
+docker build --tag "sandesham/$VERSION" .
 
 # using host network so that application can connect to mongo server on the 
 # same host and also binds application port to host port
-docker run --network=host --detach sandesham/1.0
+docker stop $(docker ps -a -q)
+docker run --env-file env.list --network=host --detach "sandesham/$VERSION"
