11316f5c0181bd8a2e3314d86084ed14ed94f613
Nithin Saji <nithin111@gmail.com>
2020-10-22 23:06:42 +0700

    remove console logs

--- src/helpers/roueUtils.ts
+++ src/helpers/roueUtils.ts
@@ -5,14 +5,11 @@ export function registerExtraRoutes(
   baserUri: string,
   routes: [any]
 ) {
-    console.log("registering routes", routes)
   routes.forEach((route) => {
-      console.log(route)
     if (route.method == "GET") {
       router.get(`${baserUri}/${route.path}`, route.handler);
     }
     if (route.method == "POST") {
-      console.log(route, `${baserUri}/${route.path}`);
 
       router.post(`${baserUri}/${route.path}`, route.handler);
     }
