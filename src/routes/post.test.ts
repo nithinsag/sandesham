95bc1c4ade89375137af566d233f4455decea766
Nithin Saji <nithin111@gmail.com>
2020-10-24 22:21:34 +0700

    add test watch

---
+++ src/routes/post.test.ts
@@ -0,0 +1,8 @@
+import supertest from 'supertest'
+import {Server} from '../server'
+
+describe('should support posting content', ()=>{
+    it('should return posts', async done=> {
+       done(); 
+    });
+})
