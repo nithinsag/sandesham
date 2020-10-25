a0ae6d03f2491a22ce75bcee7bd8a512d061834b
Nithin Saji <nithin111@gmail.com>
2020-10-25 20:13:50 +0700

    embed votes in posts and comments

--- src/models/index.ts
+++ src/models/index.ts
@@ -13,7 +13,11 @@ let UserSchema: Schema = new Schema({
 let CommunitySchema: Schema = new Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
-  visibility: { type: String, enum: ["private", "public", "restricted"], default: "public" },
+  visibility: {
+    type: String,
+    enum: ["private", "public", "restricted"],
+    default: "public",
+  },
   status: { type: String, enum: ["enabled", "disabled"], default: "enabled" },
   rules: { type: String },
   icon: { type: String },
@@ -29,9 +33,9 @@ let CommunitySchema: Schema = new Schema({
 });
 
 let PostSchema: Schema = new Schema({
-  slug: { type: String, },
+  slug: { type: String },
   title: { type: String, required: true },
-  description: { type: String, },
+  description: { type: String },
   status: { type: String, enum: ["enabled", "disabled"], default: "enabled" },
   link: String,
   type: { type: String, enum: ["link", "text", "image", "video", "gif"] },
@@ -40,6 +44,9 @@ let PostSchema: Schema = new Schema({
     user_id: { type: Schema.Types.ObjectId, ref: "User" },
     name: String,
   },
+  voteCount: { type: Schema.Types.Number, default: 0 },
+  upvotes: [Schema.Types.ObjectId],
+  downvotes: [Schema.Types.ObjectId],
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
 });
@@ -56,16 +63,11 @@ let CommunityMembership: Schema = new Schema({
     name: String,
     community: { type: Schema.Types.ObjectId, ref: "Community" },
   },
+
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
 });
 
-let PostVoteSchema: Schema = new Schema({
-  post: { type: Schema.Types.ObjectId, ref: "Post" },
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now },
-});
 
 // https://www.xuchao.org/docs/mongodb/use-cases/storing-comments.html#gsc.tab=0
 let CommentSchema: Schema = new Schema({
@@ -76,16 +78,26 @@ let CommentSchema: Schema = new Schema({
     user_id: { type: Schema.Types.ObjectId, ref: "User" },
     name: String,
   },
+  voteCount: { type: Schema.Types.Number, default: 0 },
+  upvotes: [Schema.Types.ObjectId],
+  downvotes: [Schema.Types.ObjectId],
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now },
 });
 
-let CommentVoteSchema: Schema = new Schema({
-  comment: { type: Schema.Types.ObjectId, ref: "Comment" },
-  user: { type: Schema.Types.ObjectId, ref: "User" },
-  created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now },
-});
+// TODO: Refactor when more users
+// let CommentVoteSchema: Schema = new Schema({
+//   comment: { type: Schema.Types.ObjectId, ref: "Comment" },
+//   user: { type: Schema.Types.ObjectId, ref: "User" },
+//   created_at: { type: Date, default: Date.now },
+//   updated_at: { type: Date, default: Date.now },
+// });
+// let PostVoteSchema: Schema = new Schema({
+//   post: { type: Schema.Types.ObjectId, ref: "Post" },
+//   user: { type: Schema.Types.ObjectId, ref: "User" },
+//   created_at: { type: Date, default: Date.now },
+//   updated_at: { type: Date, default: Date.now },
+// });
 
 const User = mongoose.model("User", UserSchema);
 const Community = mongoose.model("Community", CommunitySchema);
