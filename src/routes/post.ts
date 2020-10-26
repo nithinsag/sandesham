9fd7c08d32eac22cf0191445f8e4ab8a85224907
Nithin Saji <nithin111@gmail.com>
2020-10-27 04:00:29 +0700

    add upvotes and downvote routes

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -4,6 +4,7 @@ import { addCreatedBy } from "../middlewares/mongoose/author";
 import { authenticateFromHeader } from "../middlewares/authenticate";
 import { Post, Comment } from "../models";
 import restify from "express-restify-mongoose";
+import { logger } from "../helpers/logger";
 export function registerRoutes(router: Router) {
   const postUri = restify.serve(router, Post, {
     name: "post",
@@ -11,25 +12,117 @@ export function registerRoutes(router: Router) {
     preCreate: addCreatedBy,
   });
 
-  router.post(`${postUri}/:id/upvote`, authenticateFromHeader, (req, res) => {
-    if (req.user) {
-      res.json({ status: "upvoted" });
-    } else {
-      res.boom.unauthorized("User needs to be authenticated to vote!");
-    }
-  });
+  router.post(
+    `${postUri}/:id/upvote`,
+    authenticateFromHeader,
+    async (req, res) => {
+      logger.info("inside upvote");
+      if (req.user) {
+        const user_id = req.user._id;
 
-  router.post(`${postUri}/:id/downvote`, authenticateFromHeader, async (req, res) => {
-    if (req.user) {
-      Post.findOneAndUpdate({_id: req.params.id}, {
-          
-      })
-        res.json({ status: "downvoted" });
+        let post = await Post.updateOne(
+          { _id: req.params.id },
+          [
+            {
+              $set: {
+                voteCount: {
+                  $cond: [
+                    { $in: [user_id, "$upvotes"] },
+                    "$voteCount",
+                    { $add: ["$voteCount", 1] },
+                  ],
+                },
+              },
+            },
+            {
+              $set: {
+                upvotes: {
+                  $cond: [
+                    { $in: [user_id, "$upvotes"] },
+                    "$upvotes",
+                    { $setUnion: ["$upvotes", [user_id]] },
+                  ],
+                },
+              },
+            },
+            {
+              $set: {
+                downvotes: {
+                  $cond: [
+                    { $in: [user_id, "$downvotes"] },
+                    { $setDifference: ["$downvotes", [user_id]] },
+                    "$downvotes",
+                  ],
+                },
+              },
+            },
+          ],
 
-    } else {
-      res.boom.unauthorized("User needs to be authenticated to vote!");
+          { new: true }
+        );
+
+        return res.json(post);
+      } else {
+        res.boom.unauthorized("User needs to be authenticated to vote!");
+      }
     }
-  });
+  );
+
+  router.post(
+    `${postUri}/:id/downvote`,
+    authenticateFromHeader,
+    async (req, res) => {
+      logger.info("inside downvote");
+      if (req.user) {
+        const user_id = req.user._id;
+
+        let post = await Post.updateOne(
+          { _id: req.params.id },
+          [
+            {
+              $set: {
+                voteCount: {
+                  $cond: [
+                    { $in: [user_id, "$downvotes"] },
+                    "$voteCount",
+                    { $add: ["$voteCount", -1] },
+                  ],
+                },
+              },
+            },
+            {
+              $set: {
+                upvotes: {
+                  $cond: [
+                    { $in: [user_id, "$upvotes"] },
+                    { $setDifference: ["$upvotes", [user_id]] },
+                    "$upvotes",
+                  ],
+                },
+              },
+            },
+            {
+              $set: {
+                downvotes: {
+                  $cond: [
+                    { $in: [user_id, "$downvotes"] },
+                    "$downvotes",
+                    { $setUnion: ["$downvotes", [user_id]] },
+                  ],
+                },
+              },
+            },
+          ],
+
+          { new: true }
+        );
+
+        return res.json(post);
+      } else {
+        res.boom.unauthorized("User needs to be authenticated to vote!");
+      }
+    }
+  );
 
   const commentUri = restify.serve(router, Comment, {
     name: "comment",
