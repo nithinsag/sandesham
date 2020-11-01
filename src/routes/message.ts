410fd3083e352314e1ca44a01101fb8a02be9c57
Nithin Saji <nithin111@gmail.com>
2020-11-01 23:43:50 +0700

    Fix messaging apis

--- src/routes/message.ts
+++ src/routes/message.ts
@@ -1,49 +1,54 @@
 import { Message, User } from '../models'
 import { authenticateFromHeader } from "../middlewares/authenticate";
+import { logger } from '../helpers/logger'
 
 export function registerRoutes(router) {
   let API_BASE_URL = "/api/v1/message/"
 
   router.post(`${API_BASE_URL}post`, authenticateFromHeader, async (req, res) => {
-    if (!req.user) return res.boom.unauthorized('User needs to be authenticated to get messages')
-    let recipient_id = req.body.recipient;
-    let recipient: any = await User.findOne({ _id: recipient_id })
-    if (recipient) {
-      let message = await Message.create({
-        text: req.body.text,
-        from: {
-          user_id: req.user._id,
-          name: req.user.name
-        },
-        recipient: {
-          user_id: recipient._id,
-          name: recipient.name
-        }
-      })
+    try {
+      if (!req.user) return res.boom.unauthorized('User needs to be authenticated to get messages')
+      let recipient_id = req.body.recipient;
+      let recipient: any = await User.findOne({ _id: recipient_id })
+      if (recipient) {
+        let message = await Message.create({
+          text: req.body.text,
+          from: {
+            user_id: req.user._id,
+            name: req.user.name
+          },
+          to: {
+            user_id: recipient._id,
+            name: recipient.name
+          }
+        })
 
-      return res.json(message)
+        return res.json(message)
+      }
+      else {
+        return res.boom.badRequest('Invalid recipient');
+      }
+    } catch (e) {
+      return res.boom.badRequest(e);
     }
-    else {
-      return res.boom.badRequest('Invalid recipient');
-    }
-
   })
   router.get(`${API_BASE_URL}get`, authenticateFromHeader, async (req, res) => {
     if (!req.user) return res.boom.unauthorized('User needs to be authenticated to get messages')
     let onlyUnread = false;
-    if (req.params.unread) {
+    logger.info(req.query)
+    if (req.query.unread) {
       onlyUnread = true
     }
     let messages;
     if (onlyUnread) {
       messages = await Message.find(
-        { 'recipient.user_id': req.user._id, read: false },
+        { 'to.user_id': req.user._id, read: false },
       )
     } else {
       messages = await Message.find({
         $or: [
           { 'from.user_id': req.user._id },
-          { 'recipient.user_id': req.user._id },
+          { 'to.user_id': req.user._id },
         ]
       })
     }
@@ -52,9 +57,9 @@ export function registerRoutes(router) {
 
   router.post(`${API_BASE_URL}:id/markRead`, authenticateFromHeader, async (req, res) => {
     if (!req.user) return res.boom.unauthorized('User needs to be authenticated to get messages')
-    let message: any = await Message.findOne({_id: req.params.id, 'recipient.user_id': req.user._id})
-    if(!message) return res.boom.badRequest('Invalid message');
+    let message: any = await Message.findOne({ _id: req.params.id, 'to.user_id': req.user._id })
+    if (!message) return res.boom.badRequest('Invalid message');
     message.read = true;
-    return await message.save();
+    return res.json(await message.save());
   })
 }
