eadddbe6a96cfff457283fea58c7a2b2b074df53
Nithin Saji <nithin111@gmail.com>
2021-02-24 11:45:41 +0530

    add jest setup

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     },
     testRegex: '(/test/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    setupFilesAfterEnv: ['./jest.setup.ts'],
     //collectCoverage: true, //BREAKS DEBUGGING. Use when needed and comment again
 };
