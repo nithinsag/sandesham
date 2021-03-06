f364607c81979f0447bd93b12754dad0c5668957
Nithin Saji <nithin111@gmail.com>
2021-03-06 22:14:17 +0530

    fix jest warning

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
+    testEnvironment: 'node',
     testRegex: '(/test/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     setupFilesAfterEnv: ['./jest.setup.ts'],
