32f004fc0d892824cb77c5dbee012b69bed02c99
Nithin Saji <nithin111@gmail.com>
2021-04-29 15:54:51 +0700

    fix seesind issue

--- src/seeders/youtube.ts
+++ src/seeders/youtube.ts
@@ -26,43 +26,49 @@ fs.createReadStream("youtube-accounts.csv")
   .on("data", (data) => results.push(data))
   .on("end", async () => {
     console.log(results);
-    for (let channel of results) {
-      let channelId = Object.values(channel)[0];
-      let url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=20`;
-      console.log(url);
-      let response = await axios.get(url);
-      let feed = response.data;
-      const videoUrlPrefix = "https://www.youtube.com/watch?v=";
-      //console.log(feed);
-      for (let item of feed.items) {
-        let posted_at = new Date(item["snippet"]["publishedAt"]);
-        // console.log(item);
-        if (
-          item["id"]["kind"] == "youtube#video" &&
-          (!lastrun || posted_at > lastrun)
-        ) {
-          let videoId = item["id"]["videoId"];
-          let url = videoUrlPrefix + videoId;
-          let title = item["snippet"]["title"];
-          let description = item["snippet"]["description"];
-          let ogData = await getOGData(url);
-          try {
-            let post = new Post({
-              title,
-              posted_at,
-              description,
-              link: url,
-              type: "link",
-              author: botUser,
-              ogData,
-            });
-            await post.save();
-          } catch (e) {
-            console.log(e);
+    try {
+      for (let channel of results) {
+        let channelId = Object.values(channel)[0];
+        let url = `https://www.googleapis.com/youtube/v3/search?key=${process.env.YOUTUBE_API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=20`;
+        console.log(url);
+        let response = await axios.get(url);
+        let feed = response.data;
+        const videoUrlPrefix = "https://www.youtube.com/watch?v=";
+        //console.log(feed);
+        for (let item of feed.items) {
+          let posted_at = new Date(item["snippet"]["publishedAt"]);
+          // console.log(item);
+          if (
+            item["id"]["kind"] == "youtube#video" &&
+            (!lastrun || posted_at > lastrun)
+          ) {
+            let videoId = item["id"]["videoId"];
+            let url = videoUrlPrefix + videoId;
+            let title = item["snippet"]["title"];
+            let description = item["snippet"]["description"];
+            let ogData = await getOGData(url);
+            console.log(`got og data for ${url} `);
+            try {
+              let post = new Post({
+                title,
+                posted_at,
+                description,
+                link: url,
+                type: "link",
+                author: botUser,
+                ogData,
+              });
+              //console.log(post);
+              //            await post.save();
+            } catch (e) {
+              console.log(e);
+            }
           }
         }
+        fs.writeFileSync(CP_FILE, new Date().toISOString());
       }
-      fs.writeFileSync(CP_FILE, new Date().toISOString());
-      mongoose.connection.close();
+    } catch (e) {
+      console.log(e);
     }
+    mongoose.connection.close();
   });
