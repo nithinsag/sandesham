309771d36bcf9babddeeeda01ab7fc6e3b11dd15
Nithin Saji <nithin111@gmail.com>
2021-04-30 00:16:04 +0700

    update que to use correct que

--- src/asyncJobs/index.ts
+++ src/asyncJobs/index.ts
@@ -1,6 +1,6 @@
 import { Queue } from "bullmq";
 import { PushMessageJob } from "./worker";
-export const messageQue = new Queue("messageSender");
+export const messageQue = new Queue(`${process.env.DD_ENV}messageSender`);
 
 export async function addJobs(data: PushMessageJob) {
   await messageQue.add("pushMessage", data);
