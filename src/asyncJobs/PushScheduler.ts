87a915506395e9652788b18a750e35d8bcb51db2
Nithin Saji <nithin111@gmail.com>
2021-05-02 23:08:33 +0700

    add scheduled pushes

--- src/asyncJobs/PushScheduler.ts
+++ src/asyncJobs/PushScheduler.ts
@@ -6,22 +6,32 @@ import _ from "lodash";
 import mongoose from "mongoose";
 import cron from "node-cron";
 
-const job = cron.schedule("0 20 * * *", PromoteTopPost, { scheduled: false });
+const job1 = cron.schedule("0 20 * * *", getSchedulerFunction(4), {
+  scheduled: false,
+});
+const job2 = cron.schedule("0 14 * * *", getSchedulerFunction(8), {
+  scheduled: false,
+});
 
-console.log("After job instantiation");
-// job.start();
+job1.start();
+job2.start();
 
-async function PromoteTopPost() {
+function getSchedulerFunction(period) {
+  return function () {
+    PromoteTopPost(period);
+  };
+}
+async function PromoteTopPost(period) {
   connectToMongo();
   let users = await User.find();
   let tokens = users.map((user) => user.pushMessageToken).filter(Boolean);
-  let promotionalMessage = await getPromotionalMessage();
+  let promotionalMessage = await getPromotionalMessage(period);
 
   _.chunk(tokens, 400).forEach(async (batch) => {
-    let title = `${promotionalMessage.author.displayname}'s post is trending now ulkka! 🚀🚀🚀`;
+    let title = `${promotionalMessage.author.displayname}'s post is trending now on Ulkka! 🚀🚀🚀`;
     let text = promotionalMessage.title;
     let postLink = `/post/${promotionalMessage._id}`;
-    sendMulticastNotification(batch, title, text, {
+    await sendMulticastNotification(batch, title, text, {
       type: "post",
       link: postLink,
     });
@@ -29,14 +39,12 @@ async function PromoteTopPost() {
   await mongoose.connection.close();
 }
 
-async function getPromotionalMessage() {
+async function getPromotionalMessage(period) {
   let topPosts = await Post.find({
-    created_at: { $gt: new Date(Date.now() - 24 * 60 * 60 * 1000) },
+    created_at: { $gt: new Date(Date.now() - period * 60 * 60 * 1000) },
   })
     .sort({ voteCount: -1, commentCount: -1 })
     .limit(1);
   console.log(topPosts);
   return topPosts[0];
 }
-
-PromoteTopPost();
